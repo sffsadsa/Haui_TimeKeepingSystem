d41451b7cbb3235dd57092a6869d84960d5f8ed2
Tuan Anh <trananh260697@gmail.com>
2023-10-23 16:32:25 +0700

    Create project

---
+++ Haui_TimeKeepingSystem/Haui_TimeKeepingSystem/wdHistory.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Haui_TimeKeepingSystem
+{
+    /// <summary>
+    /// Interaction logic for wdHistory.xaml
+    /// </summary>
+    public partial class wdHistory : Window
+    {
+        public wdHistory()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExportExCell_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
